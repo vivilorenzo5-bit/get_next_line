6b3d849f79fdf073467ce9612943f67f318e9690
Vicente Lourenco Prista Von Bonhorst <vlourenc@c1r3s2.42lisboa.com>
2026-04-30 19:24:45 +0100

    done

--- get_next_line.c
+++ get_next_line.c
@@ -1,30 +1,13 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*   get_next_line.c                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: vlourenc <vlourenc@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2026/04/30 17:13:51 by vlourenc          #+#    #+#             */
-/*   Updated: 2026/04/30 17:25:27 by vlourenc         ###   ########.fr       */
+/*   Created: 2026/04/30 17:34:13 by vlourenc          #+#    #+#             */
+/*   Updated: 2026/04/30 17:53:19 by vlourenc         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef GET_NEXT_LINE_H
-# define GET_NEXT_LINE_H
-
-# include <stdlib.h>
-# include <unistd.h>
-
-# ifndef BUFFER_SIZE
-#  define BUFFER_SIZE 42
-# endif
-
-char	*get_next_line(int fd);
-size_t	ft_strlen(const char *s);
-char	*ft_strchr(const char *s, int c);
-char	*ft_strjoin(char *s1, char *s2);
-char	*substr(char *s, unsigned int start, size_t len);
-char	*ft_strdup(const char *s1);
-
-#endif
+#include "get_next_line.h"
---
+++ get_next_line_utils.c
@@ -0,0 +1,59 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_utils.c                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: vlourenc <vlourenc@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/04/30 17:55:07 by vlourenc          #+#    #+#             */
+/*   Updated: 2026/04/30 18:10:48 by vlourenc         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "get_next_line.h"
+
+size_t	ft_strlen(const char *s)
+{
+	size_t	i;
+
+	i = 0;
+	if (!s)
+		return (0);
+	while (s[i])
+		i++;
+	return (i);
+}
+
+char	*ft_strchr(const char *s, int c)
+{
+	int	i;
+
+	i = 0;
+	if (!s)
+		return (NULL);
+	while (s[i])
+	{
+		if (s[i] == (char)c)
+			return ((char *)s);
+		i++;
+	}
+	if ((char)c == '\0')
+		return ((char *)s);
+	return (NULL);
+}
+
+char	*ft_strjoin(char *s1, char *s2)
+{
+	char	*str;
+	size_t	i;
+	size_t	j;
+
+	if (!s1)
+	{
+		s1 = malloc(1);
+		s1[0] == '\0';
+	}
+	if (!s1 || !s2)
+		return (NULL);
+	return (str[i] = '\0', free(s1), str);
+}
